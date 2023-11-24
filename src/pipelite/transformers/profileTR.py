aecdc559b9e644e02608b60608056d40107f4433
datacorner <benoit@datacorner.fr>
2023-11-24 17:49:11 +0100

    new profile release

--- src/pipelite/transformers/profileTR.py
+++ src/pipelite/transformers/profileTR.py
@@ -6,11 +6,21 @@ import pipelite.constants as C
 from pipelite.baseobjs.BOTransformer import BOTransformer
 from pipelite.plDatasets import plDatasets
 import json
+import os
+import pathlib
+from jinja2 import Template
+import importlib.resources
 
 CFGFILES_DSOBJECT = "profileTR.json"
 PARAM_PROFILEFILE = "filename"
+PARAM_PROFILEDIR = "directory"
 PARAM_MAXVALUECOUNTS = "maxvaluecounts"
+PARAM_OUTPUT = "output"
 DEFAULT_PROFILEFILE = "pipelite.profile"
+OUTPUT_JSON = "json"
+OUTPUT_HTML = "html"
+DEFAULT_OUTPUT = OUTPUT_JSON
+OUTPUT_ACCEPTED = [ OUTPUT_JSON, OUTPUT_HTML ]
 
 class profileTR(BOTransformer):
 
@@ -33,7 +43,12 @@ class profileTR(BOTransformer):
         try:
             self.profileFile = params.getParameter(PARAM_PROFILEFILE)
             self.maxvaluecounts = params.getParameter(PARAM_MAXVALUECOUNTS)
-            if (len(self.profileFile) == 0):
+            self.output = params.getParameter(PARAM_OUTPUT, DEFAULT_OUTPUT)
+            self.directory = params.getParameter(PARAM_PROFILEDIR, C.EMPTY)
+            if (not self.output in OUTPUT_ACCEPTED):
+                self.log.error("The output specified {} is not aauthorized".format(self.output))
+                return False
+            if (self.profileFile == C.EMPTY and self.output == OUTPUT_JSON):
                 self.profileFile = DEFAULT_PROFILEFILE
             return True
         except Exception as e:
@@ -49,19 +64,41 @@ class profileTR(BOTransformer):
             etlDatasets: same as input
         """
         try :
+            self.log.info("Profile the Data Sources ...")
             globalprofile = []
             for index, input in enumerate(dsTransformerInputs):
                 self.log.info("Profiling Data Source {} ...".format(input.id))
                 subprofile = {}
                 profile = input.profile(maxvaluecounts=self.maxvaluecounts)
-                subprofile['source'] = input.id
+                subprofile['id'] = input.id
                 subprofile['profile'] = profile
                 globalprofile.append(subprofile) 
             finalProfile = {}
             finalProfile["sources"] = globalprofile
-            with open(self.profileFile, "w") as outfile: 
-                json.dump(finalProfile, outfile)
-            self.log.info("The data profile has been successfully generated in the file {}".format(self.profileFile))
+            # Create the Directory if not exist
+            if (not os.path.isdir(self.directory)):
+                self.log.info("Create the directory {}".format(self.directory))
+                os.makedirs(self.directory)
+            # Export the result in a JSON file
+            if (self.output == OUTPUT_JSON):
+                self.log.info("Export the profile as a JSON file ...")
+                fullFilename = os.path.join(self.directory, self.profileFile)
+                with open(fullFilename, "w") as outfile: 
+                    json.dump(finalProfile, outfile)
+                self.log.info("The JSON data profile has been successfully generated in the file {}".format(self.profileFile))
+            elif (self.output == OUTPUT_HTML):
+                self.log.info("Export the profile as a HTML file ...")
+                # Create the home.html page with the profiling basics infos
+                templateFile = str(importlib.resources.files(C.RESOURCE_TEMPLATES_ROOT).joinpath(C.RESOURCE_TEMPLATE_PROFILE))
+                templateContent = pathlib.Path(templateFile).read_text()
+                j2_template = Template(templateContent)
+                htmlContent = j2_template.render(profiles=finalProfile, 
+                                                 sample=input.head(10)) # profiles = Jinja2 variable inside the template file
+                fullFilename = os.path.join(self.directory, "profile-index.html")
+                with open(fullFilename, "w") as fileIndex:
+                    fileIndex.write(htmlContent)
+                    self.log.info("The HTML data profile {} has been successfully generated in the directory {}".format(fullFilename, self.directory))
+
             return plDatasets()
         except Exception as e:
             self.log.error("{}".format(str(e)))
